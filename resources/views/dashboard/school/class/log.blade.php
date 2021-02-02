dd46249c0dce446e737a5ffa5b2e5304fabd2a62
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 14:43:15 +0200

    Update log.blade.php
    De tradus și la term .... nu înțeleg la ce se referă

--- resources/views/dashboard/school/class/log.blade.php
+++ resources/views/dashboard/school/class/log.blade.php
@@ -23,7 +23,7 @@
                     </button>
                     <div class="my-1 text-center">
                         <p class="text-white">
-                            <strong>{{ __('Error: ') }}</strong> {{ session('error') }}
+                            <strong>Eroare:</strong> {{ session('error') }}
                         </p>
                     </div>
                 </div>
@@ -31,8 +31,8 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12 col-lg-6">
-                        <h5>{{ __('Add a new mark') }}</h5>
-                        <p class="text-muted">{{ __('Here you can add a new mark to the student for.') }}</p>
+                        <h5>Adaugă o notă</h5>
+                        <p class="text-muted">De aici poți adăuga note elevilor clasei tale. Sperăm că sunt note mari :)</p>
                     </div>
 
                     <div class="col-md-12 col-lg-6">
@@ -55,7 +55,7 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="student" class="text-md-left">{{ __('Select the student') }}</label>
+                                        <label for="student" class="text-md-left">Selectează elevul</label>
                                         <select id="student" name="student" class="form-control @error('student') is-invalid @enderror" required autofocus>
                                             @foreach($students as $student)
                                                 <option value="{{ $student->user->id }}">{{ $student->user->name }}</option>
@@ -69,7 +69,7 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="subject" class="text-md-left">{{ __('Select the subject') }}</label>
+                                        <label for="subject" class="text-md-left">Selectează materia</label>
                                         <select id="subject" name="subject" class="form-control @error('subject') is-invalid @enderror" required autofocus>
                                             @foreach($subjects as $subject)
                                                 <option value="{{ $subject->id }}">{{ $subject->name }}</option>
@@ -83,9 +83,9 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="mark" class="text-md-left">{{ __('Enter the mark (from 1 - 10)') }}</label>
+                                        <label for="mark" class="text-md-left">Introdu nota pe care o acorzi (de la 1 la 10)</label>
 
-                                        <input id="mark" type="text" class="form-control @error('mark') is-invalid @enderror" name="mark" value="{{ old('mark') }}" placeholder="{{ __('Enter mark...') }}" required autocomplete="mark" autofocus>
+                                        <input id="mark" type="text" class="form-control @error('mark') is-invalid @enderror" name="mark" value="{{ old('mark') }}" placeholder="Introdu nota aici..." required autocomplete="mark" autofocus>
                                         @error('mark')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -94,9 +94,9 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="date" class="text-md-left">{{ __('Enter the date') }}</label>
+                                        <label for="date" class="text-md-left">Introdu data:</label>
 
-                                        <input id="date" type="date" class="form-control @error('date') is-invalid @enderror" name="date" value="{{ old('date') }}" placeholder="{{ __('Enter here the date...') }}" required>
+                                        <input id="date" type="date" class="form-control @error('date') is-invalid @enderror" name="date" value="{{ old('date') }}" placeholder="Introdu data aici..." required>
                                         @error('date')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -106,7 +106,7 @@
                                 </div>
 
                                 <div class="card-footer">
-                                    <button type="submit" class="btn btn-gray">{{ __('Add') }}</button>
+                                    <button type="submit" class="btn btn-gray">Adaugă</button>
                                 </div>
                             </form>
                         </div>
@@ -117,8 +117,8 @@
 
                 <div class="row">
                     <div class="col-md-12 col-lg-6">
-                        <h5>{{ __('Add a new absence') }}</h5>
-                        <p class="text-muted">{{ __('Here you can add a new absence to the student for.') }}</p>
+                        <h5>Adaugă o absență</h5>
+                        <p class="text-muted">De aici poți introduce absențele elevului.</p>
                     </div>
 
                     <div class="col-md-12 col-lg-6">
@@ -140,7 +140,7 @@
                                         @enderror
                                     </div>
                                     <div class="form-group">
-                                        <label for="student" class="text-md-left">{{ __('Select the student') }}</label>
+                                        <label for="student" class="text-md-left">Selectează studentul:</label>
                                         <select id="student" name="student" class="form-control @error('student') is-invalid @enderror" required autofocus>
                                             @foreach($students as $student)
                                                 <option value="{{ $student->user->id }}">{{ $student->user->name }}</option>
@@ -153,7 +153,7 @@
                                         @enderror
                                     </div>
                                     <div class="form-group">
-                                        <label for="subject" class="text-md-left">{{ __('Select the subject') }}</label>
+                                        <label for="subject" class="text-md-left">Selectează materia:</label>
                                         <select id="subject" name="subject" class="form-control @error('subject') is-invalid @enderror" required autofocus>
                                             @foreach($subjects as $subject)
                                                 <option value="{{ $subject->id }}">{{ $subject->name }}</option>
@@ -166,9 +166,9 @@
                                         @enderror
                                     </div>
                                     <div class="form-group">
-                                        <label for="date_absence" class="text-md-left">{{ __('Enter the date') }}</label>
+                                        <label for="date_absence" class="text-md-left">Introdu data</label>
 
-                                        <input id="date_absence" type="date" class="form-control @error('date_absence') is-invalid @enderror" name="date_absence" value="{{ old('date_absence') }}" placeholder="{{ __('Enter here the date...') }}" required>
+                                        <input id="date_absence" type="date" class="form-control @error('date_absence') is-invalid @enderror" name="date_absence" value="{{ old('date_absence') }}" placeholder="Introdu data aici..." required>
                                         @error('date_absence')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -178,7 +178,7 @@
                                 </div>
 
                                 <div class="card-footer">
-                                    <button type="submit" class="btn btn-gray">{{ __('Add') }}</button>
+                                    <button type="submit" class="btn btn-gray">Marchează absența</button>
                                 </div>
                             </form>
                         </div>
