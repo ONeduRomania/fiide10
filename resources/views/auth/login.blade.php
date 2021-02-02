ca86f778b7bd9ff7879fe1fd3dcd23085cea215c
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 13:21:29 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.auth')
 
 @section('link')
-    <a class="btn btn-outline-white" href="{{ route('register') }}">{{ __('You don\'t have an account? Register here!') }} <i class="fas fa-user-plus"></i></a>
+    <a class="btn btn-outline-white" href="{{ route('register') }}">Nu deții un cont? Click aici! <i class="fas fa-user-plus"></i></a>
 @endsection
 
 @section('content')
 <div class="container-fluid px-5 py-5">
     <div class="mb-5 text-center">
         <h5 class="text-royal">
-            <strong>FiiDe10</strong>.EduManager.{{ __('Login') }}
+            <strong>Platforma Fii de 10!</strong>
         </h5>
     </div>
 
@@ -16,8 +16,8 @@
         @csrf
 
         <div class="form-group">
-            <label for="email" class="text-md-left">{{ __('E-Mail Address') }}</label>
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="{{ __('Enter your e-mail address...') }}" required autocomplete="email" autofocus>
+            <label for="email" class="text-md-left">Adresa de e-mail:</label>
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -27,13 +27,13 @@
 
         <div class="form-group">
             <label for="password" class="text-md-left">
-                {{ __('Password') }}
+                Parola
                 <a href="{{ route('password.request') }}" class="font-weight-lighter text-decoration-none">
-                    <small>Forgot your password? Reset it.</small>
+                    <small>Ai uitat parola?</small>
                 </a>
             </label>
 
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="{{ __('Enter your password...') }}" required autocomplete="current-password">
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  required autocomplete="current-password">
             @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -46,13 +46,13 @@
                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                 <label class="form-check-label">
-                    {{ __('Remember my credentials') }}
+                    Ține-mă minte!
                 </label>
             </div>
         </div>
 
         <button type="submit" class="btn btn-outline-royal btn-block">
-            {{ __('Submit') }}
+           Conectare
         </button>
     </form>
 
@@ -61,13 +61,13 @@
     <div class="row">
         <div class="col-md-12 col-lg-6 mt-1">
             <a class="btn btn-royal btn-block">
-                {{ __('Login with your Facebook account') }}
+              Loghează-te cu Facebook
                 <i class="fab fa-facebook-f"></i>
             </a>
         </div>
         <div class="col-md-12 col-lg-6 mt-1">
             <a class="btn btn-royal btn-block">
-                {{ __('Login with your Google account') }}
+                Loghează-te cu Google
                 <i class="fab fa-google"></i>
             </a>
         </div>
