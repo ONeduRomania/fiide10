fa5a7c5be1d1b365bbac0854d7947ba26bdabf91
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 13:32:31 +0200

    Update show.blade.php

--- resources/views/dashboard/admin/schools/show.blade.php
+++ resources/views/dashboard/admin/schools/show.blade.php
@@ -21,14 +21,14 @@
                     </button>
                     <div class="my-1 text-center">
                         <p class="text-white">
-                            <strong>{{ __('Error: ') }}</strong> {{ session('error') }}
+                            <strong>Eroare: </strong> {{ session('error') }}
                         </p>
                     </div>
                 </div>
             @endif
             <div class="container">
-                <h5>{{ __('Show school data') }}: {{ $school->name }}</h5>
-                <p class="text-muted">{{ __('From here you can edit the school\'s data or see the school\'s data.') }}</p>
+                <h5>Numele școlii:: {{ $school->name }}</h5>
+                <p class="text-muted">Aici poți vizualiza sau edita școala.</p>
 
                 <div class="row">
                     <div class="col-lg-6 col-md-12">
@@ -38,9 +38,9 @@
                                 @method('PATCH')
                                 <div class="card-body">
                                     <div class="form-group">
-                                        <label for="name" class="text-md-left">{{ __('Name of the institution') }}</label>
+                                        <label for="name" class="text-md-left">Numele școlii:</label>
 
-                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="{{ __('Enter institution\'s full name...') }}" required autocomplete="name" autofocus>
+                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"  required autocomplete="name" autofocus>
                                         @error('name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -49,9 +49,9 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="email_contact" class="text-md-left">{{ __('E-Mail Address') }}</label>
+                                        <label for="email_contact" class="text-md-left">Adresa de e-mail:</label>
 
-                                        <input id="email_contact" type="email" class="form-control @error('email_contact') is-invalid @enderror" name="email_contact" value="{{ old('email_contact') }}" placeholder="{{ __('Enter institution\'s e-mail address...') }}" required autocomplete="email_contact" autofocus>
+                                        <input id="email_contact" type="email" class="form-control @error('email_contact') is-invalid @enderror" name="email_contact" value="{{ old('email_contact') }}" required autocomplete="email_contact" autofocus>
                                         @error('email_contact')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -60,9 +60,9 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="address_type" class="text-md-left">{{ __('Address') }}</label>
+                                        <label for="address_type" class="text-md-left">Adresa:</label>
 
-                                        <input id="address_type" type="text" class="form-control @error('address_type') is-invalid @enderror" name="address_type" value="{{ old('address_type') }}" placeholder="{{ __('Enter institution\'s address...') }}" autocomplete="address_type" autofocus>
+                                        <input id="address_type" type="text" class="form-control @error('address_type') is-invalid @enderror" name="address_type" value="{{ old('address_type') }}" autocomplete="address_type" autofocus>
                                         @error('address_type')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -71,9 +71,9 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="phone_number" class="text-md-left">{{ __('Phone number') }}</label>
+                                        <label for="phone_number" class="text-md-left">Număr de telefon:</label>
 
-                                        <input id="phone_number" type="text" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ old('phone_number') }}" placeholder="{{ __('Enter institution\'s phone number...') }}" autocomplete="phone_number" autofocus>
+                                        <input id="phone_number" type="text" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ old('phone_number') }}"  autocomplete="phone_number" autofocus>
                                         @error('phone_number')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -82,7 +82,7 @@
                                     </div>
                                 </div>
                                 <div class="card-footer">
-                                    <button type="submit" class="btn btn-gray">{{ __('Edit') }}</button>
+                                    <button type="submit" class="btn btn-gray">Editează</button>
                                 </div>
                             </form>
                         </div>
@@ -91,7 +91,7 @@
                     <div class="col-lg-6 col-md-12">
                         <div class="card shadow-lg">
                             <div class="card-body d-flex justify-content-center">
-                                <small class="text-muted">{{ __('If you want to edit more details about this school you can go right here: ') }}<a class="text-decoration-none text-primary" href="">Edit <i class="fas fa-edit"></i></a></small>
+                                <small class="text-muted">Poți edita mai multe detalii despre această unitate de învățământ:<a class="text-decoration-none text-primary" href="">Editează<i class="fas fa-edit"></i></a></small>
                             </div>
                         </div>
                     </div>
