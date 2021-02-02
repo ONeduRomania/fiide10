0db34d157beb81a73b4cbe35c55918a0a13cc34c
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 15:01:58 +0200

    Update footer-component.blade.php

--- resources/views/components/footer-component.blade.php
+++ resources/views/components/footer-component.blade.php
@@ -2,10 +2,8 @@
     <div class="container my-5">
         <div class="row">
             <div class="col-md-12 col-lg-3 text-center my-3">
-               <img loading="lazy" class="aligncenter wp-image-55" src="https://onedu.ro/wp-content/uploads/2021/02/Platforma-Fii-de-10-09.png" alt="" width="80" height="80">
-                <p class="text-white-50">
-                    {!!  __('&copy; 2021 Comunitatea ONedu România') !!}
-                </p>
+               <img loading="lazy" class="aligncenter wp-image-55" src="https://onedu.ro/wp-content/uploads/2021/02/Platforma-Fii-de-10-09.png" alt="" width="100" height="100">
+
             </div>
             <div class="col-md-12 col-lg-3 text-center my-3">
                 <div class="font-weight-bold text-white-50">{{ __('Legal') }}</div>
@@ -29,6 +27,9 @@
                 <p class="text-white">
                     {{ __('Pagina a fost încărcată în: ') }} {{ round(microtime(true) - LARAVEL_START, 3) }}s
                     <br/>
+                                    <p class="text-white-50">
+                    {!!  __('&copy; 2021 Comunitatea ONedu România') !!}
+                </p>
                     <a class="text-white text-decoration-none" href="">{{ __('Change language to Romanian') }}</a>
                 </p>
             </div>
