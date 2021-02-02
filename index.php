a5b4a2b205043875ca694cce6ede678d5da97057
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 14:23:52 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+//echo 'We come back soon';
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register composer
+|--------------------------------------------------------------------------
+|
+| Composer provides a generated class loader for the application.
+|
+*/
+
+require __DIR__.'/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Load framework
+|--------------------------------------------------------------------------
+|
+| This bootstraps the framework and loads up this application.
+|
+*/
+
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Process request
+|--------------------------------------------------------------------------
+|
+| Execute the request and send the response back to the client.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+   $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
