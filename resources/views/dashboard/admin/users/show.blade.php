07471e80b889d18d113d3b7b4f8d62f0c4d70899
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 14:14:31 +0200

    Update show.blade.php

--- resources/views/dashboard/admin/users/show.blade.php
+++ resources/views/dashboard/admin/users/show.blade.php
@@ -21,14 +21,13 @@
                     </button>
                     <div class="my-1 text-center">
                         <p class="text-white">
-                            <strong>{{ __('Error: ') }}</strong> {{ session('error') }}
+                            <strong>Eroare:</strong> {{ session('error') }}
                         </p>
                     </div>
                 </div>
             @endif
             <div class="container">
-                <h5>{{ __('Show users data') }}: {{ $user->name }}</h5>
-                <p class="text-muted">{{ __('From here you can edit his data or see his data.') }}</p>
+                <h5>Utilizator: {{ $user->name }}</h5>
 
                 <div class="row">
                     <div class="col-lg-6 col-md-12">
@@ -39,9 +38,9 @@
                                     
                                 <div class="card-body">
                                     <div class="form-group">
-                                        <label for="name" class="text-md-left">{{ __('Full name') }}</label>
+                                        <label for="name" class="text-md-left">Numele:</label>
 
-                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="{{ __('Enter user\'s full name...') }}" required autocomplete="name" autofocus>
+                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                                         @error('name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -50,9 +49,9 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="email" class="text-md-left">{{ __('E-Mail Address') }}</label>
+                                        <label for="email" class="text-md-left">E-mail:</label>
 
-                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="{{ __('Enter user\'s e-mail address...') }}" required autocomplete="email" autofocus>
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                         @error('email')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -61,7 +60,7 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="role" class="text-md-left">{{ __('Select user\'s role') }}</label>
+                                        <label for="role" class="text-md-left">Selectează rolul:</label>
                                         <select id="role" name="role" class="form-control @error('role') is-invalid @enderror" id="role" required autofocus>
                                             @foreach($roles as $role)
                                                 <option value="{{ $role->name }}">{{ $role->name }}</option>
@@ -76,10 +75,10 @@
 
                                     <div class="form-group">
                                         <label for="password" class="text-md-left">
-                                            {{ __('Password') }}
+                                          Parola:
                                         </label>
 
-                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="{{ __('Enter user\'s password...') }}" required autocomplete="current-password">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  required autocomplete="current-password">
                                         @error('password')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -89,14 +88,14 @@
 
                                     <div class="form-group">
                                         <label for="password-confirm" class="text-md-left">
-                                            {{ __('Confirm your password') }}
+                                            Confirmă parola:
                                         </label>
 
-                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="{{ __('Enter user\'s password again...') }}" required autocomplete="new-password">
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  }}" required autocomplete="new-password">
                                     </div>
                                 </div>
                                 <div class="card-footer">
-                                    <button type="submit" class="btn btn-gray">{{ __('Edit') }}</button>
+                                    <button type="submit" class="btn btn-gray">Trimite</button>
                                 </div>
                             </form>
                         </div>
