df5220fcb96bc0efd8b9e673677d4e3d402a1b3a
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 12:21:02 +0200

    Update footer-component.blade.php

--- resources/views/components/footer-component.blade.php
+++ resources/views/components/footer-component.blade.php
@@ -2,34 +2,32 @@
     <div class="container my-5">
         <div class="row">
             <div class="col-md-12 col-lg-3 text-center my-3">
-                <h3 class="text-white"><strong>FiiDe10</strong>.EduManager</h3>
+               <img loading="lazy" class="aligncenter wp-image-55" src="https://onedu.ro/wp-content/uploads/2021/02/Platforma-Fii-de-10-09.png" alt="" width="80" height="80">
                 <p class="text-white-50">
-                    {!!  __('&copy; 2020 to Vrinceanu Radu-Tudor. All rights reserved for the content.') !!}
+                    {!!  __('&copy; 2021 Comunitatea ONedu România') !!}
                 </p>
             </div>
             <div class="col-md-12 col-lg-3 text-center my-3">
-                <div class="font-weight-bold text-white-50">{{ __('Legal & terms URLs') }}</div>
+                <div class="font-weight-bold text-white-50">{{ __('Legal') }}</div>
                 <div class="d-flex flex-column">
-                    <a href="{{ route('legal.terms') }}" class="text-decoration-none"><small class="text-white">{{ __('Terms and conditions') }}</small></a>
-                    <a href="{{ route('legal.privacy') }}" class="text-decoration-none"><small class="text-white">{{ __('Privacy policy') }}</small></a>
-                    <a href="{{ route('legal.rules') }}" class="text-decoration-none"><small class="text-white">{{ __('Internal regulation') }}</small></a>
+                    <a href="{{ route('legal.terms') }}" class="text-decoration-none"><small class="text-white">{{ __('Termenii și condițiile') }}</small></a>
+                    <a href="{{ route('legal.privacy') }}" class="text-decoration-none"><small class="text-white">{{ __('Politica de confidențialitate') }}</small></a>
+                    <a href="{{ route('legal.rules') }}" class="text-decoration-none"><small class="text-white">{{ __('Contribuitori') }}</small></a>
                 </div>
             </div>
             <div class="col-md-12 col-lg-3 text-center my-3">
-                <div class="font-weight-bold text-white-50">{{ __('Parteners & Social URLs') }}</div>
+                <div class="font-weight-bold text-white-50">{{ __('Social') }}</div>
                 <div class="d-flex flex-column">
-                    <a href="https://onedu.ro" class="text-decoration-none"><small class="text-white">{{ __('ONedu Romania Community') }}</small></a>
-                    <a href="https://web365.ro" class="text-decoration-none"><small class="text-white">{{ __('web365 Hosting Services') }}</small></a>
+                    <a href="https://onedu.ro" class="text-decoration-none"><small class="text-white">{{ __('Comunitatea ONedu România') }}</small></a>
                     <div class="btn-group" role="group">
-                        <a class="btn text-white" href="https://facebook.com">Facebook <i class="fab fa-facebook-f"></i></a>
-                        <a class="btn text-white" href="https://instagram.com">Instagram <i class="fab fa-instagram"></i></a>
+                        <a class="btn text-white" href="https://facebook.com/ONeduRomania">Facebook <i class="fab fa-facebook-f"></i></a>
+                        <a class="btn text-white" href="https://instagram.com/fiide10.ro">Instagram <i class="fab fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
             <div class="col-md-12 col-lg-3 text-center my-3">
-                <div class="font-weight-bold text-white-50">{{ __('Details') }}</div>
                 <p class="text-white">
-                    {{ __('Page was rendered in: ') }} {{ round(microtime(true) - LARAVEL_START, 3) }}s
+                    {{ __('Pagina a fost încărcată în: ') }} {{ round(microtime(true) - LARAVEL_START, 3) }}s
                     <br/>
                     <a class="text-white text-decoration-none" href="">{{ __('Change language to Romanian') }}</a>
                 </p>
