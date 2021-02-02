cf3038a1ad3d27bb2529ceb58b28644f7ee26224
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 13:13:56 +0200

    Update confirm.blade.php

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid px-5 py-5">
     <div class="mb-5 text-center">
         <h5 class="text-royal">
-            <strong>FiiDe10</strong>.EduManager.{{ __('Confirmation') }}
+            <strong>Platforma Fii de 10!</strong>
         </h5>
     </div>
 
@@ -13,9 +13,9 @@
 
         <div class="form-group">
             <label for="password" class="text-md-left">
-                {{ __('Password') }}
+                Parola
                 <a href="{{ route('password.request') }}" class="font-weight-lighter text-decoration-none">
-                    <small>Forgot your password? Reset it.</small>
+                    <small>Ai uitat parola? Reseteaz-o.</small>
                 </a>
             </label>
             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -27,7 +27,7 @@
             @enderror
         </div>
         <button type="submit" class="btn btn-primary btn-block">
-            {{ __('Confirm Password') }}
+           Confirmă parola
         </button>
     </form>
 </div>
