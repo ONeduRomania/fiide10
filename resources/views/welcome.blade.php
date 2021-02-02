c2e110df518e8dc498a98fd35f72c43b304ab1b9
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 12:43:06 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,77 +9,78 @@
 @section('content')
     <x-navbar-component class="navbar navbar-expand-md fixed-top navbar-dark bg-transparent">
         <li class="nav-item">
-            <a class="nav-link" href="#feature">{{ __('Features') }}</a>
+            <a class="nav-link" href="#despre">Despre</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#usage">{{ __('Correct usage') }}</a>
+            <a class="nav-link" href="#scoala">Înscrie școala</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#about">{{ __('About us') }}</a>
+            <a class="nav-link" href="#contact">Contact</a>
         </li>
     </x-navbar-component>
 
     <section class="section-main d-flex align-items-center">
         <div class="container text-center">
-            <p class="text-white">
-                <span class="badge badge-pill badge-royal">{{ __('New') }}</span>
-                <small class="text-white-50">{{ __('Happy to manage all the stuff! New modules available.') }}</small>
-            </p>
-            <h3 class="text-white"><strong>FiiDe10</strong>.EduManager</h3>
-            <h6 class="text-white">{{ __('Education management tool based on modules to help school staff, teachers and students.') }}</h6>
-            <a class="mt-2 btn btn-royal @auth d-none @endauth" href="{{ route('register') }}">Create your account now</a>
+               <img loading="lazy" class="aligncenter wp-image-55" src="https://onedu.ro/wp-content/uploads/2020/12/Platforma-Fii-de-10-03.png" alt="" width="200" height="200">
+            <h3 class="text-white"><strong>Platforma Fii de 10!</strong></h3>
+            <h6 class="text-white">{{ __('Viitorul educației românești să în mâinile tale!') }}</h6>
+            <a class="mt-2 btn btn-royal @auth d-none @endauth" href="{{ route('login') }}">Loghează-te acum!</a>   
+            <a class="mt-2 btn btn-royal @auth d-none @endauth" href="https://onedu.ro/doneaza">Donează</a>
         </div>
     </section>
 
     <div data-spy="scroll" data-target="#navbarMain" data-offset="0">
-        <section class="section-info d-flex align-items-center" id="feature">
+        <section class="section-info d-flex align-items-center" id="despre">
             <div class="container my-5">
-                <h2 class="text-center mb-3 font-weight-bold">{{ __('What are our features?') }}</h2>
+                <h2 class="text-center mb-3 font-weight-bold">{{ __('Despre proiectul Fii de 10!') }}</h2>
 
                 <div class="row justify-content-center text-center my-5">
                     <div class="col-md-12 col-lg-4 my-3">
                         <img src="{{ asset('images/chart.svg') }}" height="100" />
-                        <h5 class="font-weight-bold mt-1">{{ __('Metrics & Data') }}</h5>
+                        <h5 class="font-weight-bold mt-1">{{ __('Performanțe școlare') }}</h5>
                         <small class="text-muted">
-                            {{ __('With our modules you can easily manage your school as a school manager and your school staff (teacher, aux. staff) and also the classrooms.') }}
+                            Prin modulele platformei, elevul își poate spori performanța și vizualiza în timp real rezultatele.
                         </small>
                     </div>
                     <div class="col-md-12 col-lg-4 my-3">
                         <img src="{{ asset('images/files.svg') }}" height="100" />
-                        <h5 class="font-weight-bold mt-1">{{ __('Assets') }}</h5>
+                        <h5 class="font-weight-bold mt-1">Totul la îndemână</h5>
                         <small class="text-muted">
-                            {{ __('In your dashboard as a student / teacher you can manage your files which can be your homework or a course advisor.') }}
+                           Platforma se adaptează perfect pe dispozitivele mobile, îmbunătățind procesul educațional din clasă.
                         </small>
                     </div>
                     <div class="col-md-12 col-lg-4 my-3">
                         <img src="{{ asset('images/tasks.svg') }}" height="100" />
-                        <h5 class="font-weight-bold mt-1">{{ __('Tasks') }}</h5>
+                        <h5 class="font-weight-bold mt-1">Termene limită</h5>
                         <small class="text-muted">
-                            {{ __('We want to provide an unique user experience, so for that in your dashboard you will find all the data about you as a student and also your tasks.') }}
+                           Cu platforma Fii de 10! termenele limită nu mai sunt o problemă. Primești notificare când trebuie să trimiți tema sau raportul.
                         </small>
-                    </div>
+                    </div>     <a class="mt-2 btn btn-royal @auth d-none @endauth" href="https://10.onedu.ro">Află mai multe</a>
                 </div>
             </div>
+       
         </section>
 
-        <section class="section-usage d-flex align-items-center text-center" id="usage">
+        <section class="section-usage d-flex align-items-center text-center" id="scoala">
             <div class="container section-text my-5 w-50">
-                <h3 class="text-white font-weight-bold mb-3">{{ __('How to use our platform?') }}</h3>
+                <h3 class="text-white font-weight-bold mb-3">Adu școala ta pe platformă!</h3>
                 <p class="text-white-50">
-                    {{ __('To use our platform, please register a new account and we will send you an e-mail, you will need to confirm that. After that you will be up and running in no time. We belive that the user interface is a clear guide for you on how to use the platform.') }}
+                    Suntem transparenți cu privire la toate datele utilizate și cum facem lucrurile. Ne dorim să fim un sprijin real pentru profesori și elevi, tocmai de aceea ar trebui să te alături și tu! Numai prin utilizarea platformei în mai multe școli putem să facem lucrurile să devină realitate. Totul gratuit!
                 </p>
+                <a class="mt-2 btn btn-royal @auth d-none @endauth" href="https://10.onedu.ro/inscrie-scoala">Înscrie școala!</a>
             </div>
         </section>
 
-        <section class="section-about d-flex align-items-center" id="about">
+        <section class="section-about d-flex align-items-center" id="contact">
             <div class="container section-text my-5 w-50">
-                <h3 class="font-weight-bold mb-3 text-center">{{ __('About our team') }}</h3>
+                <h3 class="font-weight-bold mb-3 text-center">Intră în contact!</h3>
                 <div class="row">
                     <div class="col-md-12 col-lg-6 text-center">
-                        <h5 class="font-weight-bold">{{ __('Story of the application...') }}</h5>
+                        <h5 class="font-weight-bold">Contactează-ne</h5>
                         <p class="text-muted">
-                            {{ __('The application was started by Comunitatea ONedu Romania in the summer of 2020, the scope of this platform is to help schools and its staff & students to have a nice centralized platform.') }}
+                           Ne poți scrie la fiide10@onedu.ro sau poți completa un formular de contact. 
                         </p>
+                        <a class="mt-2 btn btn-royal @auth d-none @endauth" href="https://10.onedu.ro/contact">Completează formularul</a>
                     </div>
                     <div class="col-md-12 col-lg-6 text-center">
                         <img src="{{ asset('images/programming.svg') }}" height="100" />
