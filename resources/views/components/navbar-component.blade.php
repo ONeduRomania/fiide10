77edd93090c0739607e1fadca4794ae49a6e9bce
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 16:04:15 +0200

    Update navbar-component.blade.php

--- resources/views/components/navbar-component.blade.php
+++ resources/views/components/navbar-component.blade.php
@@ -1,7 +1,7 @@
 <nav id="navbarMain" {{ $attributes }}>
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            <strong>Platforma Fii de 10!</strong>
+            <strong>Fii de 10!</strong>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
