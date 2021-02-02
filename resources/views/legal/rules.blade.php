e156e51d3b2cc27f7d5cef6648ecc390e81e546e
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 11:53:49 +0200

    Update rules.blade.php

--- resources/views/legal/rules.blade.php
+++ resources/views/legal/rules.blade.php
@@ -7,6 +7,14 @@
         <div class="container my-5">
             <div class="px-5 py-5">
                 <h2 class="text-center font-weight-bold">Contribuitori</h2>
+                <p> Toate mulțumirile noastre pentru ajutorul oferit în programarea platformei se îndreaptă spre: </p>
+                <p> Radu Vrînceanu - <a href="https://github.com/iRaduS/"> GitHub</a></p>
+                <p> Lucian Ionescu - <a href="https://github.com/luciandex"> GitHub</a></p>
+                <br/>
+                <p> De asemenea mulțumim și tuturor sponsorilor și partenerilor din proiect: </p>
+                <p><a href="https://semimaratongalati.ro"> Semimaraton Galați</a></p>
+                <p><a href="https://fabine.eu"> Platforma Fă Bine</a></p>
+                <p><a href="https://mabucurdeviata.ro"> Asociația Mă Bucur de Viață</a></p>
             </div>
         </div>
     </section>
