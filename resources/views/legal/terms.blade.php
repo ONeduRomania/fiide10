dd59e4ec14ee0dbe1098bcd9447c8c1ee478c86f
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-02 19:09:32 +0200

    Update terms.blade.php

--- resources/views/legal/terms.blade.php
+++ resources/views/legal/terms.blade.php
@@ -13,14 +13,15 @@
 Comunitatea ONedu România, structură a Asociației pentru Copii cu Dizabilități- Mă Bucur de Viață, cu
 sediul social în Galați, strada Bld. Siderurgiștilor, nr. 22, bl. SD8A, ap. 6, județul Galați, înregistrată în
 Registrul Asociațiilor și Fundațiilor cu numărul 37 din data de 26.11.2018, CIF 40484090, reprezentată
-legal de Cosmin Andrei în calitate de președinte. Prin utilizarea Serviciilor noastre, sunteți de acord cu prezenții Termeni și condiții. Vă rugăm să îi citiți cu
-atenție. Serviciile noastre sunt dedicate mediului educațional, ce implică și prelucrarea de date ale minorilor și,
+legal de Cosmin Andrei în calitate de președinte.<br/> Prin utilizarea Serviciilor noastre, sunteți de acord cu prezenții Termeni și condiții. Vă rugăm să îi citiți cu
+atenție.<br/> Serviciile noastre sunt dedicate mediului educațional, ce implică și prelucrarea de date ale minorilor și,
 prin urmare, este posibil ca uneori să se aplice Termeni și condiții suplimentare sau cerințe suplimentare
 pentru produse, ce vor fi aduse la cunoștință în mod prompt înainte de accesarea acestor produse. În
-cazul anumitor Servicii,care vor fi expres menționate de către platforma Fii de 10, pot exista Termeni și
+cazul anumitor Servicii,care vor fi expres menționate de către Comunitatea ONedu România, pot exista Termeni și
 condiții suplimentare, iar, dacă utilizați acele Servicii, Termenii și condițiile suplimentare respective s-ar
 putea să facă parte din contractul între unitatea școlară din care face parte copilul dumneavoastra și
-platforma Fii de 10 sau să fie publicate pe website-ul Fii de 10 – www.fiide10.ro .</p>
+platforma Fii de 10 sau să fie publicate pe website-ul Fii de 10 – www.fiide10.ro.<br/>
+Înainte de utilizarea serviciilor noastre, vă rugăm să completați contractul de parteneriat disponibil pe pagina principală, pe care îl transmiteți la fiide10@onedu.ro, alături de acordurile în scris ale părinților elevilor cărora li se prelucrează datele.</p>
 <p><b>Utilizarea Serviciilor noastre</b></p>
 <p>Trebuie să respectați toate politicile puse la dispoziție în cadrul Serviciilor.
 Utilizarea Serviciilor nu vă oferă niciun drept de proprietate intelectuală asupra acestora sau
@@ -29,22 +30,21 @@ accesați servicii de informare, servicii de management școlar informațional, 
 de dotare cu echipamente și internet sau servicii de asistență. Nu aveți dreptul să utilizați
 conținutul din cadrul Serviciilor noastre, cu excepția cazului în care prin contractarea de către
 unitatea școlară a licenței asupra sistemului Fii de 10, deveniți utilizatori ai serviciilor noastre.
-Serviciile noastre afișează un anumit conținut care nu aparține platformei Fii de 10.
+Serviciile noastre afișează un anumit conținut care nu aparține platformei Fii de 10. <br/>
 Responsabilitatea pentru acest conținut revine exclusiv entității care îl pune la dispoziție și
 anume unitații școlare precum și utilizatorilor, în ceea ce privește modulul de mesagerie. În
 ceea ce privește utilizarea Serviciilor de către dvs., putem să vă trimitem anunțuri privind
 serviciile, mesaje administrative și alte informații. Puteți renunța la unele dintre aceste
-comunicări sau la toate, prin bifarea/debifarea acestora din Setările contului dumneavoastră Fii
-de 10.</p>
+comunicări sau la toate, prin bifarea/debifarea acestora din Setările contului dumneavoastră.</p>
 <p>La momentul începerii utilizării serviciilor platformei Fii de 10 puteți opta să fiți de acord sau nu
-ca Fii de 10 să realizeze sondaje de opinie, campanii de fidelizare și campanii de promovare în
+ca Comunitatea ONedu România să realizeze sondaje de opinie, campanii de fidelizare și campanii de promovare în
 rândul utilizatorilor folosind ca mijloace de comunicare contul de utilizator, adresa de email sau
 numărul de telefon, dezabonarea putând fi solicitată oricând la adresa de e-
 mail: fiide10@onedu.ro.</p>
 <p><b>Contul dvs. Fii de 10</b></p>
-<p>Pentru a utiliza serviciile platformei Fii de 10, este necesar un Cont Fii de 10. Prin contractarea
+<p>Pentru a utiliza serviciile platformei Fii de 10, este necesar un Cont pe platforma Fii de 10. Prin contractarea
 sistemului Fii de 10 de către unitatea şcolară, deveniți una din următoarele categorii de
-utilizatori: cadru didactic, personal secretariat, personal conducere, părinte sau elev.</p>
+utilizatori: cadru didactic, personal conducere sau elev.</p>
 
 <p>Politicile de confidențialitate Fii de 10 explică modul în care tratăm datele dvs. cu caracter
 personal și în care vă protejăm confidențialitatea atunci când utilizați Serviciile noastre.</p>
@@ -52,12 +52,12 @@ personal și în care vă protejăm confidențialitatea atunci când utilizați 
 <p>Anumite Servicii vă permit să trimiteți conținut. Fii de 10 administrează platforma de
 management școlar interactiv și modulele integrate în această platformă: catalog școlar
 electronic, mesagerie, modului dedicat secretariatului, modulul dedicat directorilor, modulul
-pentru profesor și nu intervine asupra datelor conținute de sistem.
+pentru profesor și nu intervine asupra datelor conținute de sistem.<br/>
 Anumite Servicii vă pot oferi modalități de accesare și de eliminare a conținutului furnizat către
 respectivul Serviciu. De asemenea, anumite Servicii au Termeni și condiții sau setări care
 restrâng domeniul de utilizare a conținutului trimis în cadrul respectivelor Servicii. Asigurați-vă
 că dețineți drepturile necesare pentru a putea accesa modulele corespunzătoare pentru
-întregul conținut pe care îl trimiteți către Serviciile noastre.
+întregul conținut pe care îl trimiteți către Serviciile noastre.<br/>
 Puteți afla mai multe informații despre modul în care Fii de 10 utilizează şi stochează conținutul
 în politica de confidențialitate sau în Termenii și condițiile suplimentare pentru anumite Servicii
 care vor fi puse la dispoziția dumneavoastră pe website-ul  www.fiide10.ro . Dacă trimiteți
@@ -65,8 +65,7 @@ feedback sau sugestii despre Servicii, putem utiliza feedbackul sau sugestiile r
 încălca legislația privind protecția datelor cu caracter personal.</p>
 <p><b>Despre programele software din cadrul Serviciilor</b></p>
 <p>Nu aveți dreptul să copiați, să modificați, să distribuiți, să vindeți sau să închiriați nicio parte a
-Serviciilor și a programelor software incluse; nu aveți dreptul să refaceți sau să încercați să
-extrageți codul sursă al respectivului program software, cu excepția cazurilor în care aveți
+Serviciilor și a programelor software incluse; nu aveți dreptul să refaceți codul sursă al respectivului program software, cu excepția cazurilor în care aveți
 permisiunea scrisă din partea noastră.</p>
 <p><b>Modificarea și încetarea furnizării Serviciilor</b></p>
 <p>Modificăm și îmbunătățim în mod constant Serviciile. Putem să adăugăm sau să eliminăm
@@ -77,21 +76,21 @@ de servicii către dumneavoastră să nu sufere.</p>
 cu respectarea securității, integrității și confidențialității datelor și sperăm că vă va face plăcere
 să le utilizați. Dar, există anumite lucruri pe care nu le putem promite în legătură cu Serviciile.
 Unitatea școlară este responsabilă de actualitatea și integralitatea datelor cu privire la absențe,
-note discilpine/materii predate din sistem. Pentru claritate, Fii de 10 nu are dreptul de a
+note discilpine/materii predate din sistem. Pentru claritate, Comunitatea ONedu România nu are dreptul de a
 interveni în niciun fel asupra datelor din sistem furnizate către unitatea școlară, cu excepția
-cazurilor în care Fii de 10 se angajează în mod explicit să realizeze aceasta acțiune prin contract,
-cu respectarea prevederilor legale. În orice altă situație, Fii de 10 nu este responsabil de lipsa
+cazurilor în care Comunitatea ONedu România se angajează în mod explicit să realizeze aceasta acțiune prin contract,
+cu respectarea prevederilor legale. În orice altă situație, Comunitatea ONedu România nu este responsabil de lipsa
 datelor din sistem ce ar trebui să fie furnizate de către unitatea școlară sau întârzieri privind
 completarea datelor de către unitatea școlară.</p>
 <p><b>Răspunderea pentru Serviciile noastre</b></p>
 
-<p>Fii de 10 nu va fi în niciun caz respnsabil pentru nicio pierdere sau pagubă care intră în categoria
+<p>Comunitatea ONedu România nu va fi în niciun caz respnsabil pentru nicio pierdere sau pagubă care intră în categoria
 forței majore.</p>
 
 <p><b>Despre prezenții Termeni și condiții</b></p>
 <p>Putem să modificăm prezenții Termeni și Condiții și Termenii și Condițiile suplimentare care se
 aplică unui Serviciu, de exemplu, pentru a corespunde schimbărilor survenite la nivelul
-legislației sau al Serviciilor noastre. Consultați periodic Termenii și condițiile.
+legislației sau al Serviciilor noastre. Consultați periodic Termenii și condițiile.<br/>
 Pe această pagină vom posta notificările privind modificările prezenţilor Termeni şi Condiţii.
 Vom posta notificările cu privire la modificarea Termenilor și condițiilor suplimentare în cadrul
 Serviciului în cauză. Modificările nu se vor aplica retroactiv și vor intra în vigoare în termen de
@@ -99,16 +98,14 @@ minimum paisprezece zile după postare. Totuși, modificările aferente funcții
 Serviciu și cele din motive legale vor intra în vigoare imediat. Dacă nu sunteți de acord cu
 modificările aduse Termenilor și Condiţiilor aferente unui Serviciu, vă sugerăm să întrerupeți
 utilizarea Serviciului respectiv.<br/>
- În cazul unei discrepanțe între prezenții Termeni și Condiții și Termenii și condițiile
+- În cazul unei discrepanțe între prezenții Termeni și Condiții și Termenii și condițiile
 suplimentare, vor prevala Termenii și condiţiile suplimentare.<br/>
- Prezenții Termeni și Condiții reglementează relația dintre Fii de 10 și dvs. Aceștia nu creează
+- Prezenții Termeni și Condiții reglementează relația dintre Fii de 10 și dvs. Aceștia nu creează
 niciun fel de drepturi pentru terți beneficiari.<br/>
- Dacă nu respectați prezenții Termeni și Condiții și nu luăm măsuri imediate, nu înseamnă că
+- Dacă nu respectați prezenții Termeni și Condiții și nu luăm măsuri imediate, nu înseamnă că
 renunțăm la drepturile pe care le avem (de exemplu, dreptul de a lua măsuri pe viitor).<br/>
- Dacă anumiți termeni și anumite condiții se dovedesc a fi inaplicabili, aceasta nu va afecta
-restul Termenilor și Condițiilor.<br/>
-Pentru informații despre modul în care puteți să contactați Fii de 10, accesați pagina noastră de
-contact.</p>
+- Dacă anumiți termeni și anumite condiții se dovedesc a fi inaplicabili, aceasta nu va afecta
+restul Termenilor și Condițiilor.<br/> </p>
              
             </div>
         </div>
