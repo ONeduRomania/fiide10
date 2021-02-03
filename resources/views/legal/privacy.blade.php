1b9336010228486103f5899610004ba382aa9ecd
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-03 11:18:54 +0200

    Update privacy.blade.php

--- resources/views/legal/privacy.blade.php
+++ resources/views/legal/privacy.blade.php
@@ -24,59 +24,59 @@ acordul tău. În acest sens, urmează să te contactam pentru a ne comunica hot
 <p>Comunitatea ONedu România, structură a Asociației pentru Copii cu Dizabilități- Mă Bucur de Viață, cu
 sediul social în Galați, strada Bld. Siderurgiștilor, nr. 22, bl. SD8A, ap. 6, județul Galați, înregistrată în
 Registrul Asociațiilor și Fundațiilor cu numărul 37 din data de 26.11.2018, CIF 40484090, reprezentată
-legal de Cosmin Andrei în calitate de președinte, este proprietar și administrator al site-ului www.fiide10.ro
- respectă confidențialitatea și securitatea prelucrării datelor cu caracter personal.</p>
+legal de Cosmin Andrei în calitate de președinte, este proprietar și administrator al site-ului www.fiide10.ro, care
+respectă confidențialitatea și securitatea prelucrării datelor cu caracter personal.</p>
 <p><b>IDENTITATEA ȘI DATELE DE CONTACT ALE PERSOANEI RESPONSABILE CU PROTECȚIA DATELOR</b></p>
-<p>Fii de 10 a desemnat un responsabil cu protecția datelor („RPD”). RPD-ul nostru poate fi contactat la
+<p>Comunitatea ONedu România a desemnat un responsabil cu protecția datelor („RPD”). RPD-ul nostru poate fi contactat la
 urmatoarea adresă de mail: fiide10@onedu.ro.</p>
 <p><b>DEFINIȚII:</b></p>
-<p>1. • ANSPDCP reprezintă Autoritatea Națională de Supraveghere a Prelucrării Datelor cu Caracter Personal;<br/>
-2. • &quot;date cu caracter personal&quot; înseamnă orice informație privind o persoană fizică identificată sau
+<p>• ANSPDCP reprezintă Autoritatea Națională de Supraveghere a Prelucrării Datelor cu Caracter Personal;<br/>
+• &quot;date cu caracter personal&quot; înseamnă orice informație privind o persoană fizică identificată sau
 identificabilă (&quot;persoană vizată&quot;); o persoană fizică identificabilă este o persoană care poate fi
 identificată, direct sau indirect, în special prin referire la un element de identificare, cum ar fi un nume,
 un număr de identificare, date de localizare, un identificator online, sau la unul sau mai multe elemente
 specifice, proprii identității sale fizice, fiziologice, genetice, psihice, economice, culturale sau sociale;<br/>
-3. • &quot;prelucrare&quot; înseamnă orice operațiune sau set de operațiuni efectuate asupra datelor cu caracter
+• &quot;prelucrare&quot; înseamnă orice operațiune sau set de operațiuni efectuate asupra datelor cu caracter
 personal sau asupra seturilor de date cu caracter personal, cu sau fară utilizarea de mijloace
 automatizate, cum ar fi colectarea, înregistrarea, organizarea, structurarea, stocarea, adaptarea sau
 modificarea, extragerea, consultarea, utilizarea, divulgarea prin transmitere, diseminarea sau punerea la
 dispoziție în orice alt mod, alinierea sau combinarea, restrictionarea, ștergerea sau distrugerea;<br/>
-4. • &quot;restricționarea prelucrării&quot; înseamnă marcarea datelor cu caracter personal stocate cu scopul de a
+• &quot;restricționarea prelucrării&quot; înseamnă marcarea datelor cu caracter personal stocate cu scopul de a
 limita prelucrarea viitoare a acestora;<br/>
-5. • &quot;operator&quot; înseamnă persoana fizică sau juridică, autoritatea publică, agenția sau alt organism care,
+• &quot;operator&quot; înseamnă persoana fizică sau juridică, autoritatea publică, agenția sau alt organism care,
 singur sau împreună cu altele, stabilește scopurile și mijloacele de prelucrare a datelor cu caracter
 personal; atunci când scopurile și mijloacele prelucrării sunt stabilite prin dreptul Uniunii sau dreptul
 intern, operatorul sau criteriile specifice pentru desemnarea acestuia pot fi prevăzute în dreptul Uniunii
 sau în dreptul intern;<br/>
-6. • &quot;persoana împuternicită de operator&quot; înseamnă persoana fizică sau juridică, autoritatea publică,
+• &quot;persoana împuternicită de operator&quot; înseamnă persoana fizică sau juridică, autoritatea publică,
 agenția sau alt organism care prelucrează datele cu caracter personal în numele operatorului;<br/>
-7. • &quot;destinatar&quot; înseamnă persoana fizică sau juridică, autoritatea publică, agenția sau alt organism căreia
+• &quot;destinatar&quot; înseamnă persoana fizică sau juridică, autoritatea publică, agenția sau alt organism căreia
 (căruia) îi sunt divulgate datele cu caracter personal, indiferent dacă este sau nu o parte terță. Cu toate
 acestea, autoritățile publice cărora li se pot comunica date cu caracter personal în cadrul unei anumite
 anchete în conformitate cu dreptul Uniunii sau cu dreptul intern nu sunt considerate destinatari;
 prelucrarea acestor date de către autoritățile publice respective respectă normele aplicabile în materie
 de protecție a datelor, în conformitate cu scopurile prelucrării;<br/>
-8. • &quot;consimțământ&quot; al persoanei vizate înseamnă orice manifestare de voință liberă, specifică, informată
+• &quot;consimțământ&quot; al persoanei vizate înseamnă orice manifestare de voință liberă, specifică, informată
 și lipsita de ambiguitate a persoanei vizate prin care aceasta acceptă, printr-o declarație sau printr-o
 acțiune fără echivoc, că datele cu caracter personal care o privesc să fie prelucrate.<br/>
-9.
+
 <b>DRETURILE PERSOANEI VIZATE</b><br/>
-1. • Dreptul de acces înseamnă dreptul persoanei vizate de a obține o confirmare din partea operatorului
+• Dreptul de acces înseamnă dreptul persoanei vizate de a obține o confirmare din partea operatorului
 că prelucrează sau nu datele cu caracter personal care il privesc și, în caz afirmativ, acces la datele
 respective și la informații privind modalitatea în care sunt prelucrate datele.<br/>
-2. • Dreptul la portabilitatea datelor se referă la dreptul de a primi datele personale într-un format
+• Dreptul la portabilitatea datelor se referă la dreptul de a primi datele personale într-un format
 structurat, utilizat în mod curent și care poate fi citit automat și la dreptul că aceste date să fie transmise
 direct altui operator, dacă acest lucru este fezabil din punct de vedere tehnic.<br/>
-3. • Dreptul la opoziție&quot; vizează dreptul persoanei vizate de a se opune prelucrării datelor personale
+• Dreptul la opoziție&quot; vizează dreptul persoanei vizate de a se opune prelucrării datelor personale
 atunci când prelucrarea este necesară pentru indeplinirea unei sarcini care servește unui interes public
 sau când are în vedere un interes legitim al operatorului. Atunci când prelucrarea datelor cu caracter
 personal are drept scop marketingul direct, persoana vizată are cu precadere dreptul de a se opune
 prelucrării în orice moment.<br/>
-4. • Dreptul la rectificare se referă la corectarea, fără întârzieri nejustificate, a datelor cu caracter personal
+• Dreptul la rectificare se referă la corectarea, fără întârzieri nejustificate, a datelor cu caracter personal
 inexacte stocate. Rectificarea trebuie comunicată fiecarui destinatar la care au fost transmise datele, cu
 excepția cazului în care acest lucru se dovedește imposibil sau presupune eforturi disproporționate
 (demonstrabile).<br/>
-5. • Dreptul la ștergerea datelor (&quot;dreptul de a fi uitat&quot;) înseamnă dreptul persoanei vizate de a solicita să
+• Dreptul la ștergerea datelor (&quot;dreptul de a fi uitat&quot;) înseamnă dreptul persoanei vizate de a solicita să
 îi fie șterse datele cu caracter personal, fără întârzieri nejustificate, în cazul în care se aplică unul dintre
 următoarele motive: acestea nu mai sunt necesare pentru îndeplinirea scopurilor pentru care au fost
 colectate sau prelucrate; își retrage consimțământul și nu există niciun alt temei juridic pentru
@@ -84,23 +84,24 @@ prelucrare; se opune prelucrării și nu există motive legitime care să preval
 personal au fost prelucrate ilegal; datele cu caracter personal trebuie șterse pentru respectarea unei
 obligații legale; datele cu caracter personal au fost colectate în legătură cu oferirea de servicii ale
 societății informaționale.<br/>
-6. • Dreptul la restricționarea prelucrării  poate fi exercitat în cazul în care persoana contestă exactitatea
+• Dreptul la restricționarea prelucrării  poate fi exercitat în cazul în care persoana contestă exactitatea
 datelor, pe o perioadă care permite verificarea corectitudinii datelor; prelucrarea este ilegală, iar
 persoana se opune ștergerii datelor cu caracter personal, solicitând în schimb restricționarea; în cazul în
 care operatorul nu mai are nevoie de datele cu caracter personal în scopul prelucrării, dar persoana i le
 solicită pentru constatarea, exercitarea sau apărarea unui drept în instanță; în cazul în care persoana s-a
 opus prelucrării pentru intervalul de timp în care se verifică dacă drepturile legitime ale operatorului
 prevalează asupra celor ale persoanei respective.</p>
+
 <p><b>CE TIPURI DE DATE CU CARACTER PERSONAL COLECTĂM</b></p>
 
 <p>Colectăm date cu caracter personal pentru a creea conturi de utilizator pe platforma Fii de 10, și pentru
 ca persoanele vizate (nu ne limitam la datele din cartea de identitate ale parintilor, elevilor sau a
 cadrelor didactice), să aiba acces la informațiile oferite de platforma Fii de 10.<br/>
-1. • Nume, prenume, e-mail privat, date persoană de contact în caz de urgență (nume și număr de
-telefon)<br/>
-2. • Țară, număr de telefon fix și mobil/, data nașterii, ocupație<br/>
-3. • Fotografii<br/>
-4. • Date necesare încheierii unei forme de colaborare sau angajare, conform Codului Muncii sau Codului Civil</p>
+• Elev: Numele și prenumele, adresa de e-mail, clasa;<br/>
+• Profesor: Numele și prenumele, clase, adresa de e-mail, telefon;<br/>
+• Părinte: Seria și numărul CI, numele și prenumele;<br/>
+• Școala: nume, adresă, telefon, e-mail, clase, situații școlare, materii, profesori, elevi;<br/>
+• Date necesare încheierii unei forme de colaborare și acordul prelucrării datelor minorilor;</p>
 <p><b>DE CE COLECTĂM DATELE DUMNEAVOASTRĂ CU CARACTER PERSONAL ȘI CARE ESTE TEMEIUL
 COLECTĂRII</b></p>
 <p>Este necesar să colectăm datele dumneavoastră cu caracter personal pentru a respecta prevederile
@@ -111,20 +112,20 @@ caracter personal pe care le colectăm sunt necesare pentru, încheierea, execut
 având posibilitatea de a accesa informațiile stocate în platformă.<br/>
 Dacă nu sunteți de acord ca datele dumneavoastră sa fie prelucrate, aveți dreptul și libertatea de a nu
 completa formularele disponibile și de a nu accesa site-ul www.fiide10.ro .<br/>
-În momentul completării formularelor disponibilepe www.fiide10.ro  se vor colecta date cu caracter
+În momentul completării formularelor disponibile pe www.fiide10.ro  se vor colecta date cu caracter
 personal, oferind informații cu privire la scopul colectării fiecare date personale ce se solicită.
-Fii de 10, poate primi, de asemenea, și înregistra, pe serverele sale informațiile referitoare la adresa IP și
+Comunitatea ONedu România, poate primi, de asemenea, și înregistra, pe serverele sale informațiile referitoare la adresa IP și
 informațiile cookie ale site-ului solicitat. Aveți opțiunea de a seta browserul de internet astfel încât
 acesta să respingă anumite cookies. În acest caz, va exista însă un impact negativ asupra navigării dvs. pe
-site-ul platformei Fii de 10. Politica Fii de 10 referitoare la cookie-uri poate fi consultată la
-www.fiide10.ro.<br/>
-Fii de 10 procesează datele cu caracter personal ale persoanelor vizate în baza obligațiilor legale,
+site-ul platformei Fii de 10. Politica Comunității ONedu România referitoare la cookie-uri poate fi consultată la
+www.onedu.ro.<br/>
+Comunitatea ONedu România procesează datele cu caracter personal ale persoanelor vizate în baza obligațiilor legale,
 executarea contractului dintre Fii de 10 și persoana vizată și interesul legitim al platformei Fii de 10.</p>
 
 <p><b>CÂT TIMP PĂSTRĂM DATELE CU CARACTER PERSONAL</b></p>
 <p>Păstrăm datele cu caracter personal furnizate de dumneavoastră pentru o perioadă rezonabilă de timp,
 atât cât este necesar pentru a îndeplini scopurile descrise mai sus cât și pentru a ne îndeplini obligațiile
-legale cărora ne supunem. Dacă datele cu caracter personal nu ne mai sunt necesare și folositoare
+legale cărora ne supunem. Dacă datele cu caracter personal nu ne mai sunt necesare și folositoare,
 acestea se șterg.</p>
 <p>otivele pentru care păstrăm datele cu caracter personal au la bază respectarea obligațiilor legale în
 conformitate cu Regulamentul și pentru a ne susține apărările pentru eventuale acțiuni în fața
@@ -133,26 +134,26 @@ instanțelor competente.</p>
 <p><b>DEZVALUIREA DATELOR</b></p>
 <p>În situația în care suntem obligați să dezvăluim datele dumneavoastră cu caracter personal printr-un
 ordin judecătoresc sau pentru a respecta alte cerinţe legale, printr-o notificare vă vom anunța înainte de
-a furniza aceste date exceptând cazul în care este interzisă această dezvaluire. Nu vom vinde, dezvălui,
+a furniza aceste date, exceptând cazul în care este interzisă această dezvaluire. Nu vom vinde, dezvălui,
 distribui datele dumneavoastră cu caracter personal fără a obține acordul și permisiunea
 dumneavoastră.</p>
 <p><b>SECURIZAREA DATELOR CU CARACTER PERSONAL</b></p>
-<p>Fii de 10 nu va dezvălui datele cu caracter personal colectate prin intermediul platoformei Fii de 10 cu
-excepția angajaților autorizați și contractorilor/partenerilor ce au calitatea de persoane împuternicite.
-Fii de 10, împreună cu colaboratorii săi, și-a asumat responsabilitatea implementării unor măsuri tehnice
+<p>Comunitatea ONedu România nu va dezvălui datele cu caracter personal colectate prin intermediul platoformei Fii de 10 cu
+excepția angajaților autorizați și contractorilor/partenerilor ce au calitatea de persoane împuternicite.<br/>
+Comunitatea ONedu România, împreună cu colaboratorii săi, și-a asumat responsabilitatea implementării unor măsuri tehnice
 și organizatorice corespunzătoare privind protecția confidențialității și securității datelor cu caracter
 personal, respectiv pentru protecția împotriva accesului neautorizat, a utilizării, alterării sau distrugerii
 datelor cu caracter personal conform prevederilor RGPD, după cum urmează:<br/>
-1. • Utilizatorii care au acces la baza de date a informațiilor cu caracter personal sunt doar cei care și-au
+• Utilizatorii care au acces la baza de date a informațiilor cu caracter personal sunt doar cei care și-au
 creat cont pe platforma Fii de 10, fiecare dintre aceștia accesează baza de date cu propriul nume de cont
 și parola.<br/>
-2. • Toți angajații, colaboratorii și furnizorii de servicii ai platformei Fii de 10, care intră în contact cu datele
+• Toți angajații, colaboratorii și furnizorii de servicii ai platformei Fii de 10, care intră în contact cu datele
 cu caracter personal, trebuie să acționeze în conformitate cu principiile politicilor și procedurilor de
 confidențialitate și de securitate a datelor cu caracter personal semnând declarații și acorduri de
 confidențialitate cu privire la aceste date.<br/>
-3. • Utilizatorii accesează datele cu caracter personal numai pentru monitorizarea situație școlare și doar în
+• Utilizatorii accesează datele cu caracter personal numai pentru monitorizarea situații școlare și doar în
 conformitate cu scopul declarat al colectarii datelor.<br/>
-4. • Computerele de la care se accesează baza de date a informațiilor cu caracter personal sunt parolate,
+• Computerele de la care se accesează baza de date a informațiilor cu caracter personal sunt parolate,
 au implementate soluții de protecție antivirus, antispam și firewall updatate la zi.<br/>
 Toți angajații, colaboratorii și furnizorii de servicii, care intră în contact cu datele cu caracter personal,
 trebuie să acționeze în conformitate cu principiile politicilor și procedurilor de confidențialitate și de
@@ -167,9 +168,9 @@ concordanță cu cerințele legale și în condiții de maximă siguranță și 
 <p>Avem dreptul de a modifica, completa Politica de Confidențialitate oricând, afișând Politica de
 Confidențialitate modificată pe  www.fiide10.ro .</p>
 
-<p><b>DATELE DE CONTACT PENTRU AUTORITATEA NAȚIONALĂ PENTRU SUPRAVEGHEREA PRELUCRĂRII</b></p>
-<p>DATELOR CU CARACTER PERSONAL (ANSPDCP)<br/>
-Adresa: B-dul G-ral Gheorghe Magheru 28-30, Sector 1, cod postal 010336, București, România<br/>
+<p><b>DATELE DE CONTACT PENTRU AUTORITATEA NAȚIONALĂ PENTRU SUPRAVEGHEREA PRELUCRĂRII
+DATELOR CU CARACTER PERSONAL (ANSPDCP)</b></p>
+<p>Adresa: B-dul G-ral Gheorghe Magheru 28-30, Sector 1, cod postal 010336, București, România<br/>
 Mail: anspdcp@dataprotection.ro<br/>
 Tel: +0318.059.211; +0318.059.212</p>
 
