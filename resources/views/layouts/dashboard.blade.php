29fb2ddcb3f80b3dc42e3344345e157babada42d
LucianDex <remindfwd@gmail.com>
2021-02-07 19:26:40 +0200

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,22 +6,22 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- Primary Meta Tags --}}
-    <title>{{ config('app.name', 'FiiDe10') }}</title>
-    <meta name="title" content="{{ config('app.name', 'FiiDe10.EduManager') }}">
-    <meta name="description" content="Education management tool based on modules to help school staff, teachers and students.">
+    <title>{{ config('app.name', 'Platforma Fii de 10!') }}</title>
+    <meta name="title" content="{{ config('app.name', 'Platforma Fii de 10!') }}">
+    <meta name="description" content="Digitalizăm Educație Împreună!">
 
     {{--  Open Grapgh / Facebook  --}}
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{ env('APP_URL', 'localhost:8000') }}">
-    <meta property="og:title" content="{{ config('app.name', 'FiiDe10.EduManager') }}">
-    <meta property="og:description" content="Education management tool based on modules to help school staff, teachers and students.">
+    <meta property="og:title" content="{{ config('app.name', 'Fii De 10!') }}">
+    <meta property="og:description" content="Instrument de management al educației bazat pe module pentru a ajuta personalul școlii, profesorii și elevii.">
     <meta property="og:image" content="">
 
     {{--  Twitter  --}}
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="{{ env('APP_URL', 'localhost:8000') }}">
-    <meta property="twitter:title" content="{{ config('app.name', 'FiiDe10.EduManager') }}">
-    <meta property="twitter:description" content="Education management tool based on modules to help school staff, teachers and students.">
+    <meta property="twitter:title" content="{{ config('app.name', 'Fii De 10!') }}">
+    <meta property="twitter:description" content="Instrument de management al educației bazat pe module pentru a ajuta personalul școlii, profesorii și elevii.">
     <meta property="twitter:image" content="">
 
     {{--  Scripts  --}}
@@ -36,40 +36,40 @@
         <div id="app">
             <x-navbar-component class="navbar navbar-expand-md navbar-dark bg-royal">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('home') }}">{{ __('Dashboard') }}</a>
+                    <a class="nav-link" href="{{ route('home') }}">{{ __('Panou de control') }}</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a id="adminDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ __('Admin Control Panel') }} <span class="caret"></span>
+                        {{ __('Panou de control Admin') }} <span class="caret"></span>
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="adminDropdown">
                         @can('manage-users')
                         <a class="dropdown-item text-md-center" href="{{ route('users.index') }}">
-                            {{ __('Manage all the users') }} <i class="fas fa-users-cog"></i>
+                            {{ __('Administrează toți utilizatorii') }} <i class="fas fa-users-cog"></i>
                         </a>
                         @endcan
 
                         <a class="dropdown-item text-md-center" href="{{ route('schools.index') }}">
-                            {{ __('Manage all the schools') }} <i class="fas fa-address-book"></i>
+                            {{ __('Administrează toate școlile') }} <i class="fas fa-address-book"></i>
                         </a>
                     </div>
                 </li>
                 <li class="nav-item dropdown">
                     <a id="schoolDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ __('School Control Panel') }} <span class="caret"></span>
+                        {{ __('Panou de control Școală') }} <span class="caret"></span>
                     </a>
                     
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="schoolDropdown">
                         @can('manage-users')
                             <a class="dropdown-item text-md-center" href="{{ route('users.index') }}">
-                                {{ __('Manage all the users') }} <i class="fas fa-users-cog"></i>
+                                {{ __('Administrează toți utilizatorii') }} <i class="fas fa-users-cog"></i>
                             </a>
                         @endcan
 
                         <a class="dropdown-item text-md-center" href="{{ route('schools.index') }}">
-                            {{ __('Manage all the schools') }} <i class="fas fa-address-book"></i>
+                            {{ __('Administrează toate școlile') }} <i class="fas fa-address-book"></i>
                         </a>
                     </div>
                 </li>
