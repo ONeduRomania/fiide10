397ba9e9b8bd67cc799d623953d9699d27007190
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-09 15:27:59 +0200

    Update home.blade.php

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 <div class="section-info my-5">
-
+@can('manage-users')
+                        <a class="dropdown-item text-md-center" href="{{ route('users.index') }}">	                        <a class="dropdown-item text-md-center" href="{{ route('users.index') }}">
+                            {{ __('Manage all the users') }} <i class="fas fa-users-cog"></i>	                            {{ __('Administrează toți utilizatorii') }} <i class="fas fa-users-cog"></i>
+                        </a>	                        </a>
+                        @endcan	                        @endcan
 </div>
 @endsection
