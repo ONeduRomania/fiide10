38e5ed287d2866a37a86ed84d892136306e1dfe4
LucianDex <remindfwd@gmail.com>
2021-02-18 23:43:18 +0200

    Added Log class and migration

---
+++ app/Log.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+class Log extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['data'];
+
+    public static function allWithCache(Carbon $datetime, int $perpage, int $whichpage, int $schoolId) {
+        return Cache::tags('classrooms')->remember('classrooms_page_' . $whichpage, $datetime, function () use ($perpage, $schoolId) {
+            return self::with('masterTeacher')->where('school_id', $schoolId)->paginate($perpage);
+        });
+    }
+
+    public function masterTeacher() {
+        return $this->belongsTo('App\User', 'master_teacher');
+    }
+}
