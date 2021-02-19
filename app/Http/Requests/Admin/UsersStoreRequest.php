ddce1830151b0f02fd34ed1dc6ae168a011ffda8
Cosmin Andrei <50593959+cosmin-andrei@users.noreply.github.com>
2021-02-19 09:11:11 +0200

    Update UsersStoreRequest.php

--- app/Http/Requests/Admin/UsersStoreRequest.php
+++ app/Http/Requests/Admin/UsersStoreRequest.php
@@ -14,7 +14,7 @@ class UsersStoreRequest extends FormRequest
     public function authorize()
     {
         // @todo when done with permissions update this...
-        $user->givePermissionTo('manage-users');
+        $user->can('manage users');
         return true;
     }
 
