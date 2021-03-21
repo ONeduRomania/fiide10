c1d73506fdaae4d9012cb480756d05b9f4d1fec9
derTuca <salexandru.tuca@outlook.com>
2021-03-21 17:15:57 +0200

    Delete unused handler

--- app/Observers/SubmittedHomeworkObserver.php
+++ app/Observers/SubmittedHomeworkObserver.php
@@ -7,17 +7,6 @@ use App\SubmittedHomework;
 class SubmittedHomeworkObserver
 {
     /**
-     * Handle the User "created" event.
-     *
-     * @param SubmittedHomework $submission
-     * @return void
-     */
-    public function created(SubmittedHomework $submission)
-    {
-        // TODO: Notify the teacher
-    }
-
-    /**
      * Handle the User "deleted" event.
      *
      * @param SubmittedHomework $submission
